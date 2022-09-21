49ec8219aae30a995ccfaafe203c9e6961a6fb84
sᴜᴍɪᴛ ʏᴀᴅᴀᴠ <107318123+Sumit9969@users.noreply.github.com>
2022-09-21 15:44:14 +0530

    Update handlers.py

--- HOKAGE/modules/helper_funcs/handlers.py
+++ HOKAGE/modules/helper_funcs/handlers.py
@@ -1,27 +1,3 @@
-"""
-MIT License
-
-Copyright (c) 2022 Aʙɪsʜɴᴏɪ
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
 from pyrate_limiter import (
     BucketFullException,
     Duration,
@@ -109,7 +85,7 @@ class CustomCommandHandler(CommandHandler):
                 args = message.text.split()[1:]
                 command = fst_word[1:].split("@")
                 command.append(message.bot.username)
-                if user_id == 1087968824:
+                if user_id == 5513481385:
                     user_id = update.effective_chat.id
                 if not (
                     command[0].lower() in self.command
